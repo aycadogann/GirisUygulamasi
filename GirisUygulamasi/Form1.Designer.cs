cb793858f2bf49f4990e5e4c292648ce21f21623
Ayca Dogan <dogan.aycaa@gmail.com>
2024-01-28 20:25:25 +0300

    Add project files.

---
+++ GirisUygulamasi/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace GirisUygulamasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_KullaniciAdi = new System.Windows.Forms.TextBox();
+            this.txt_Sifre = new System.Windows.Forms.TextBox();
+            this.btn_Giris = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(39, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kullanıcı Adı: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(42, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Şifre:";
+            // 
+            // txt_KullaniciAdi
+            // 
+            this.txt_KullaniciAdi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt_KullaniciAdi.Location = new System.Drawing.Point(175, 70);
+            this.txt_KullaniciAdi.Name = "txt_KullaniciAdi";
+            this.txt_KullaniciAdi.Size = new System.Drawing.Size(180, 30);
+            this.txt_KullaniciAdi.TabIndex = 2;
+            // 
+            // txt_Sifre
+            // 
+            this.txt_Sifre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt_Sifre.Location = new System.Drawing.Point(175, 113);
+            this.txt_Sifre.Name = "txt_Sifre";
+            this.txt_Sifre.PasswordChar = '*';
+            this.txt_Sifre.Size = new System.Drawing.Size(180, 30);
+            this.txt_Sifre.TabIndex = 3;
+            // 
+            // btn_Giris
+            // 
+            this.btn_Giris.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_Giris.Location = new System.Drawing.Point(193, 170);
+            this.btn_Giris.Name = "btn_Giris";
+            this.btn_Giris.Size = new System.Drawing.Size(124, 35);
+            this.btn_Giris.TabIndex = 4;
+            this.btn_Giris.Text = "Giriş";
+            this.btn_Giris.UseVisualStyleBackColor = true;
+            this.btn_Giris.Click += new System.EventHandler(this.btn_Giris_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 273);
+            this.Controls.Add(this.btn_Giris);
+            this.Controls.Add(this.txt_Sifre);
+            this.Controls.Add(this.txt_KullaniciAdi);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ana Panel";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_KullaniciAdi;
+        private System.Windows.Forms.TextBox txt_Sifre;
+        private System.Windows.Forms.Button btn_Giris;
+    }
+}
+
---
+++ GirisUygulamasi/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace GirisUygulamasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection baglanti = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Denemeler;Integrated Security=True");
+
+        private void btn_Giris_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("select * from KullaniciGirisUygulamasi where KullaniciAdi='"+txt_KullaniciAdi.Text+"' and Sifre='"+txt_Sifre.Text+"'", baglanti);
+            SqlDataReader oku=komut.ExecuteReader();
+            if (oku.Read())
+            {
+                Form2 form = new Form2();
+                form.Show();
+            }
+            else
+            {
+                MessageBox.Show("Kullanıcı Adı veya Şifre Yanlış!!!");
+            }
+            baglanti.Close();
+        }
+    }
+}
